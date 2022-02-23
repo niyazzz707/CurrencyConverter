b4e2b1bc3877fbee2771483b23b04d40040f04b3
niyaz <niyazbek1134@gmail.com>
2022-02-24 02:15:36 +0600

    Create Class of getters and setters for variables

---
+++ src/com/company/Currency.java
@@ -0,0 +1,70 @@
+package com.company;
+
+public class Currency {
+    private float usd_rub;
+ private float usd_kzt;
+ private float usd_eur;
+ private float eur_kzt;
+ private float eur_rub;
+ private float rub_kzt;
+
+    // USD TO RUB
+    public float getUsd_rub() {
+        return usd_rub;
+    }
+
+    public void setUsd_rub(float usd_rub) {
+        this.usd_rub = usd_rub;
+    }
+    // USD TO RUB
+
+    // USD TO KZT
+    public float getUsd_kzt() {
+        return usd_kzt;
+    }
+
+    public void setUsd_kzt(float usd_kzt) {
+        this.usd_kzt = usd_kzt;
+    }
+    // USD TO KZT
+
+    // USD TO EUR
+    public float getUsd_eur() {
+        return usd_eur;
+    }
+
+    public void setUsd_eur(float usd_eur) {
+        this.usd_eur = usd_eur;
+    }
+    // USD TO RUB
+
+    // EUR TO KZT
+    public float getEur_kzt() {
+        return eur_kzt;
+    }
+
+    public void setEur_kzt(float eur_kzt) {
+        this.eur_kzt = eur_kzt;
+    }
+    // EUR TO KZT
+
+    // EUR TO RUB
+    public float getEur_rub() {
+        return eur_rub;
+    }
+
+    public void setEur_rub(float eur_rub) {
+        this.eur_rub = eur_rub;
+    }
+    // EUR TO RUB
+
+    // EUR TO KZT
+    public float getRub_kzt() {
+        return rub_kzt;
+    }
+
+    public void setRub_kzt(float rub_kzt) {
+        this.rub_kzt = rub_kzt;
+    }
+    // EUR TO KZT
+}
--- src/com/company/PostgresDB.java
+++ src/com/company/PostgresDB.java
@@ -1,17 +1,17 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PostgresDB implements IDB {
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
-        String connectionURL="jdbc:postgresql://localhost:5432/currency";
+        String connectionURL = "jdbc:postgresql://localhost:5432/currency";
+        String username = "postgres";
+        String password = "niyazbek";
         try {
             Class.forName("org.postgresql.Driver");
 
-            Connection con= DriverManager.getConnection(connectionURL,"postgres","niyazbek");
+            Connection con = DriverManager.getConnection(connectionURL, username, password);
 
             return con;
         } catch (Exception e) {
